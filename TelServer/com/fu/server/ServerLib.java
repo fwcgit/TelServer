d86bdb4471d612fc3e20640b10b69381befae106
伏维成 <fuweicheng@tomato.shop>
2018-11-19 13:12:07 +0800

    bug

---
+++ TelServer/com/fu/server/ServerLib.java
@@ -0,0 +1,37 @@
+package com.fu.server;
+import java.io.File;
+public class ServerLib{
+
+	static{
+		//System.loadLibrary("server");
+		File file = new File("/home/linux/Desktop/TelServer/src/com/fu/server/libserver.so");
+		System.load(file.getAbsolutePath());
+	}
+
+	public native void starpServer(int port);
+
+	public native void closeServer();
+
+	public native void sendData(String session,byte[] data);
+
+	public native void sendCmd(String session,byte cmd);
+
+	public native void closeClient(int session);
+
+	public native String[] getClientList();
+
+	public void newClientConnect(String session)
+	{
+		System.out.println("jni_newClientConnect"+session);
+	}
+
+	public void closeClinetConnect(String  session)
+	{
+		System.out.println("jni_closeClinetConnect"+session);
+	}
+	
+	public  void LOG(int type,String txt)
+	{
+		System.out.println("log->type:"+type+"----txt"+txt);
+	}
+}
